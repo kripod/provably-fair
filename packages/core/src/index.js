6a6d408bdbb12d20bc449ff50f66942973e3ea72
Kristóf Poduszló <kripod@protonmail.com>
2017-03-10 11:48:29 +0100

    Improve accuracy of core docs

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -6,8 +6,8 @@ import { createHmac } from 'crypto';
  * @param {number} max Maximum value of output (excluded).
  * @param {number} size Size of parsable chunks in bytes. Must be less than or equal to 6.
  * @param {number} [startOffset=0] Where to start reading the buffer.
- * @returns {number} An integer number parsed from the given buffer. If no appropriate number can be
- * parsed, `NaN` is returned.
+ * @returns {number} An unsigned integer parsed from the given buffer. If no appropriate number can
+ * be parsed, `NaN` is returned.
  */
 export function parseRandomUInt(buf, max, size, startOffset = 0) {
   // No appropriate number can be parsed
@@ -21,7 +21,7 @@ export function parseRandomUInt(buf, max, size, startOffset = 0) {
 /**
  * Generates a random integer based on the given seeds, using the given HMAC algorithm.
  * @param {number} size Size of output in bytes. Must be less than or equal to 6.
- * @param {string} hmacAlgorithm Hash algorithm used for computing the HMAC of the given seed pair.
+ * @param {string} hmacAlgorithm Algorithm used for computing the HMAC of the given seed pair.
  * @param {string|Buffer} secretSeed Secret seed used as HMAC key.
  * @param {string|Buffer} [publicSeed=] Public seed used as HMAC data. To prove fairness of random
  * outputs, the hash of `secretSeed` shall be known before revealing `publicSeed`.
@@ -47,13 +47,8 @@ export function randomInt(
   const maxRange = 256 ** size;
 
   // Validate parameters
-  if (size <= 0 || size > 6) {
-    throw new RangeError('Size must be non-negative and less than or equal to 6.');
-  }
-
-  if (range <= 0 || range > maxRange) {
-    throw new RangeError('Max - min must be non-negative and less than 256 ** size.');
-  }
+  if (size <= 0 || size > 6) throw new RangeError('Size must be non-negative and less than or equal to 6.');
+  if (range <= 0 || range > maxRange) throw new RangeError('Max - min must be non-negative and less than or equal to 256 ** size.');
 
   // Create HMAC buffer
   const buf = createHmac(hmacAlgorithm, secretSeed).update(publicSeed).digest();
